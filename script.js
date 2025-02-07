008a94680f71be086ad03854e10ba22068f9e2f7
Suraj Giri <155833064+surajixcp@users.noreply.github.com>
2025-02-07 18:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
-    //Write your code here
-
-
+    const boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(element => {
+        element.style.color = 'rgb(0, 128, 0)'; // Change to green
+    });
 }
-
 
 function return_normal() {
-    //Write your code here
-
-    
+    const boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(element => {
+        element.style.color = 'rgb(0, 0, 0)'; // Revert to black
+    });
 }
+
